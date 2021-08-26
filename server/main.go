6bc007a15d2d4d6a2315ecede1edbb26712375e0
jiangyouhua <jingo1997@163.com>
2021-08-26 18:01:15 +0800

    add webSocket

--- server/main.go
+++ server/main.go
@@ -3,13 +3,40 @@ package main
 import (
 	"log"
 	"net/http"
+	"server/socket"
+)
+
+const (
+	keyFile = "/Users/jiangyouhua/code/system/live/server/server.key"
+	certFile = "/Users/jiangyouhua/code/system/live/server/server.crt"
+)
+
+var (
+	hub *socket.Hub
 )
 
 func main() {
-	keyFile := "/Users/jiangyouhua/code/system/live/server/server.key"
-	certFile := "/Users/jiangyouhua/code/system/live/server/server.crt"
-	http.Handle("/", http.FileServer(http.Dir("./html/")))
+	hub = socket.NewHub()
+	go hub.Run()
+
+	// 分开处理webSocket、site。
+	http.HandleFunc("/", webSite)
+	http.HandleFunc("/ws", webSocket)
+
 	if err := http.ListenAndServeTLS(":443", certFile, keyFile, nil); err != nil {
 		log.Fatalln(err)
 	}
+}
+
+func webSite(w http.ResponseWriter, r * http.Request) {
+	p := "." + r.URL.Path
+	if p == "./" {
+		p = "./site/index.html"
+	}
+	http.ServeFile(w, r, p)
+}
+
+func webSocket(w http.ResponseWriter, r * http.Request) {
+	log.Println("socket", r.URL)
+	socket.ServeWs(hub, w, r)
 }
